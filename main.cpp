5bca78c3001c18ee7beefb7dc2565a495333cbeb
AndrusovN <andrusov.n@gmail.com>
2022-10-28 00:35:36 +0300

    add all functions without testing them

--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <gtest/gtest.h>
 #include "string.h"
 
+void CheckLastSymbol(const String& s) {
+    ASSERT_EQ('\0', s.data()[s.size()])
+}
+
 TEST(ConstructorTests, CstyleStringLength) {
     String s("abcd");
     ASSERT_EQ(4, s.length());
@@ -14,7 +18,7 @@ TEST(ConstructorTests, CstyleStringValue) {
     for(size_t i = 0; i < 4; ++i) {
         ASSERT_EQ(cstring[i], s[i]);
     }
-    ASSERT_EQ('\0', s.data()[size]);
+    check_last_symbol();
 }
 
 TEST(ConstructorTests, SizeNCharLength) {
@@ -33,7 +37,7 @@ TEST(ConstructorTests, SizeNCharValue) {
     for(size_t i = 0; i < size; ++i) {
         ASSERT_EQ(value, s[i]);
     }
-    ASSERT_EQ('\0', s.data()[size]);
+    check_last_symbol();
 }
 
 TEST(OperatorTests, EqualityEQ) {
@@ -77,7 +81,7 @@ TEST(ConstructorTests, Copy) {
     String s2 = s1;
     
     ASSERT_EQ(s1, s2);
-    ASSERT_EQ('\0', s2.data()[s2.size()]);
+    check_last_symbol(s2);
 }
 
 TEST(OperatorTests, Assign) {
@@ -85,7 +89,7 @@ TEST(OperatorTests, Assign) {
     s2 = s1;
 
     ASSERT_EQ(s1, s2);
-    ASSERT_EQ('\0', s2.data()[s2.size()]);
+    check_last_symbol(s2);
 }
 
 TEST(OperatorTests, ComparisonLess) {
@@ -188,6 +192,489 @@ TEST(OperatorTests, BracketsNonConstExceptionAboveSize) {
 
     ASSERT_THROW(s[4], IndexOutOfRangeException*);
     ASSERT_THROW(s[1791791791], IndexOutOfRangeException*);
+}
+
+TEST(OperatorTests, PlusEqTwoNotEmpty) {
+    String s1("ab"), s2("oba");
+    s1 += s2;
+
+    ASSERT_EQ(String("aboba"), s1);
+    check_last_symbol(s1);
+}
+
+TEST(OperatorTests, PlusEqSecondEmpty) {
+    String s1("cringe"), s2;
+    s1 += s2;
+
+    ASSERT_EQ(String("cringe"), s1);
+    check_last_symbol(s1);
+}
+
+TEST(OperatorTests, PlusEqFirstEmpty) {
+    String s1, s2("some");
+    s1 += s2;
+
+    ASSERT_EQ(s2, s1);
+    check_last_symbol(s1);
+}
+
+TEST(OperatorTests, PlusEqTwoEmpty) {
+    String s1, s2;
+    s1 += s2;
+
+    ASSERT_EQ(s2, s1);
+    check_last_symbol(s1);
+}
+
+TEST(OperatorTests, PlusEqSame) {
+    String s1("aba");
+    s1 += s1;
+
+    ASSERT_EQ(String("abaaba"), s1);
+    check_last_symbol(s1);
+}
+
+TEST(OperatorTests, PlusTwoNotEmpty) {
+    String s1("ab"), s2("oba");
+
+    auto result = s1 + s2;
+    ASSERT_EQ(String("aboba"), result);
+    check_last_symbol(result);
+}
+
+TEST(OperatorTests, PlusSame) {
+    String s1("aba");
+
+    auto result = s1 + s1;
+    ASSERT_EQ(String("abaaba"), result);
+    check_last_symbol(result);
+}
+
+TEST(OperatorTests, PlusEmpty) {
+    String s1("aba"), s2;
+
+    auto result1 = s1 + s2;
+    ASSERT_EQ(s1, result1);
+    check_last_symbol(result1);
+    
+    auto result2 = s2 + s1;
+    ASSERT_EQ(s1, result2);
+    check_last_symbol(result2);
+}
+
+TEST(OperatorTests, PlusChar) {
+    String s("abob");
+    char c = 'a';
+
+    ASSERT_EQ(String("aboba"), s + c);
+    ASSERT_EQ(String("aabob"), c + s);
+    check_last_symbol(s + c);
+    check_last_symbol(c + s);
+}
+
+TEST(OperatorTests, PlusCharEmptyString) {
+    String s;
+    char c = 'a';
+
+    ASSERT_EQ(String("a"), s + c);
+    ASSERT_EQ(String("a"), c + s);
+    check_last_symbol(s + c);
+    check_last_symbol(c + s);
+}
+
+TEST(OperatorTests, PlusCharTwice) {
+    String s("ab");
+
+    auto result1 = s + 'b' + 'a';
+    ASSERT_EQ(String("abba"), result1);
+    check_last_symbol(result1);
+
+    auto result2 = 'b' + ('a' + s);
+    ASSERT_EQ(String("baab"), result2);
+    check_last_symbol(result2);
+}
+
+TEST(OperatorTests, PlusEqChar) {
+    String s("cring");
+    s += 'e';
+
+    ASSERT_EQ(String("cringe"), s);
+    check_last_symbol(s);
+}
+
+TEST(OperatorTests, PlusEqCharEmpty) {
+    String s;
+    s += 'a';
+
+    ASSERT_EQ(String("a"), s);
+    check_last_symbol(s);
+}
+
+TEST(OperatorTests, PlusEqTwice) {
+    String s1("ab"), s2("ob"), s3("a");
+    
+    (s1 += s2) += s3;
+
+    ASSERT_EQ(String("aboba"), s1);
+    check_last_symbol(s1);
+}
+
+TEST(MethodTests, LengthEmpty) {
+    String s;
+
+    ASSERT_EQ(0, s.length());
+}
+
+TEST(MethodTests, LengthFull) {
+    String s("cringe");
+
+    ASSERT_EQ(6, s.length());
+}
+
+TEST(MethodTests, PushBack) {
+    String s("cring");
+    s.push_back('e');
+
+    ASSERT_EQ(String("cringe"), s);
+    check_last_symbol(s);
+}
+
+TEST(MethodTests, PushBackNull) {
+    String s("cringe");
+    s.push_back('\0');
+
+    String s_check("cringea");
+    s[6] = '\0';
+    String s_wrong("cringe");
+    ASSERT_EQ(s_check, s);
+    ASSERT_NEQ(s_wrong, s);
+    check_last_symbol(s);
+}
+
+TEST(MethodTests, PushBackEmpty) {
+    String s;
+    s.push_back('c');
+
+    ASSERT_EQ(String("c"), s);
+    check_last_symbol(s);
+}
+
+TEST(MethodTests, PopBack) {
+    String s("cringea");
+    s.pop_back();
+
+    ASSERT_EQ(String("cringe"), s);
+    check_last_symbol(s);
+}
+
+TEST(MethodTests, PopBackEmpty) {
+    String s;
+    ASSERT_THROW(s.pop_back(), EmptyStringCallException*);
+}
+
+TEST(MethodTests, FrontConst) {
+    const String s("cringe");
+
+    ASSERT_EQ('c', s.front());
+}
+
+TEST(MethodTests, FrontConstEmpty) {
+    const String s;
+
+    ASSERT_THROW(s.front(), EmptyStringCallException*);
+}
+
+TEST(MethodTests, Front) {
+    String s("cringe");
+
+    ASSERT_EQ('c', s.front());
+}
+
+TEST(MethodTests, FrontChange) {
+    String s("kringe");
+    s.front() = 'c';
+
+    ASSERT_EQ(String("cringe"), s);
+}
+
+TEST(MethodTests, FrontEmpty) {
+    String s;
+
+    ASSERT_THROW(s.front(), EmptyStringCallException*);
+}
+
+TEST(MethodTests, BackConst) {
+    String s("cringe");
+
+    ASSERT_EQ('e', s.back());
+    check_last_symbol(s);
+}
+
+TEST(MethodTests, BackConstEmpty) {
+    String s;
+
+    ASSERT_THROW(s.back(), EmptyStringCallException*);
+}
+
+TEST(MethodTests, Back) {
+    String s("cringe");
+
+    ASSERT_EQ('e', s.back());
+    check_last_symbol(s);
+}
+
+TEST(MethodTests, BackEmpty) {
+    String s;
+
+    ASSERT_THROW(s.back(), EmptyStringCallException*);
+}
+
+TEST(MethodTests, BackChange) {
+    String s("cringa");
+    s.back() = 'e';
+
+    ASSERT_EQ(String("cringe"), s);
+    check_last_symbol(s);
+}
+
+TEST(MethodTests, FindSingle) {
+    String s("test");
+
+    ASSERT_EQ(1, s.find(String("es")));
+}
+
+TEST(MethodTests, FindMultiple) {
+    String s("aaaaa");
+    
+    ASSERT_EQ(0, s.find(String("a")));
+}
+
+TEST(MethodTests, FindNone) {
+    String s("test");
+
+    ASSERT_EQ(4, s.find(String("aboba")));
+}
+
+TEST(MethodTests, RFindSingle) {
+    String s("test");
+
+    ASSERT_EQ(1, s.rfind(String("es")));
+}
+
+TEST(MethodTests, RFindMultiple) {
+    String s("aaaaa");
+
+    ASSERT_EQ(4, s.rfind(String("a")));
+}
+
+TEST(MethodTests, RFindNone) {
+    String s("test");
+
+    ASSERT_EQ(4, s.rfind(String("aboba")));
+}
+
+TEST(MethodTests, Substr) {
+    String s("test");
+    
+    auto result = s.substr(1, 2);
+    ASSERT_EQ(String("es"), result);
+    check_last_symbol(result);
+}
+
+TEST(MethodTests, SubstrEmpty) {
+    String s("test");
+
+    auto result = s.substr(1, 0);
+    ASSERT_EQ(String(), result);
+    check_last_symbol(result);
+}
+
+TEST(MethodTests, SubstrOnEmpty) {
+    String s;
+
+    ASSERT_ANY_THROW(s.substr(0, 1));
+}
+
+TEST(MethodTests, SubstrEmptyOnEmpty) {
+    String s;
+
+    ASSERT_NO_THROW(s.substr(0, 0));
+    
+    auto result = s.substr(0, 0);
+    ASSERT_EQ(String(), result);
+    check_last_symbol(result);
+}
+
+TEST(MethodTests, SubstrEmptyOnEmptyNotZero) {
+    String s;
+
+    ASSERT_ANY_THROW(s.substr(1, 0));
+}
+
+TEST(MethodTests, SubstrNegative) {
+    String s("test");
+
+    ASSERT_ANY_THROW(s.substr(2, -1));
+}
+
+TEST(MethodTests, SubstrTooLong) {
+    String s("test");
+
+    ASSERT_THROW(s.substr(1, 4), IndexOutOfRangeException*);
+}
+
+TEST(MethodTests, SubstrFromNegative) {
+    String s("test");
+
+    ASSERT_THROW(s.substr(-1, 2), IndexOutOfRangeException*);
+}
+
+TEST(MethodTests, EmptyFalse) {
+    String s("test");
+
+    ASSERT_FALSE(s.empty());
+}
+
+TEST(MethodTests, EmptyTrue) {
+    String s;
+
+    ASSERT_TRUE(s.empty());
+}
+
+TEST(MethodTests, EmptyFromEmptyCharset) {
+    String s("");
+
+    ASSERT_TRUE(s.empty());
+}
+
+TEST(MethodTests, Clear) {
+    String s("test");
+    s.clear();
+
+    ASSERT_TRUE(s.empty());
+    ASSERT_EQ(String(), s);
+    ASSERT_EQ(s.get_capacity(), 5);
+    check_last_symbol(s);
+}
+
+TEST(MethodTests, ClearEmpty) {
+    String s;
+    ASSERT_NO_THROW(s.clear());
+    s.clear();
+
+    ASSERT_TRUE(s.empty());
+    ASSERT_EQ(String(), s);
+    ASSERT_EQ(s.get_capacity(), 0);
+    check_last_symbol(s);
+}
+
+TEST(MethodTests, ShrinkToFit) {
+    String s("test");
+    s.push_back('c');
+    s.shrink_to_fit();
+
+    ASSERT_EQ(6, s.get_capacity());
+    ASSERT_EQ(String("testc"), s);
+    ASSERT_EQ(s.size(), s.capacity());
+    check_last_symbol(s);
+}
+
+TEST(MethodTests, ShrinkToFitEmpty) {
+    String s;
+    s.shrink_to_fit();
+
+    ASSERT_EQ(0, s.get_capacity());
+    ASSERT_EQ(String(), s);
+    ASSERT_EQ(s.size(), s.capacity());
+    check_last_symbol(s);
+}
+
+TEST(MethodTests, Data) {
+    String s1("test");
+    String s2(s1.data());
+
+    ASSERT_EQ(s1, s2);
+    check_last_symbol(s2);
+}
+
+TEST(MethodTests, DataTwice) {
+    String s("test");
+    char* data1 = s.data();
+    char* data2 = s.data();
+
+    ASSERT_EQUALS(data1, data2);
+}
+
+TEST(MethodTests, Size) {
+    String s("test");
+
+    ASSERT_EQ(4, s.size());
+}
+
+TEST(MethodTests, SizeEmpty) {
+    String s;
+
+    ASSERT_EQ(0, s.size());
+}
+
+TEST(MethodTests, SizeExtended) {
+    String s("test");
+    s.push_back('c');
+
+    ASSERT_EQ(5, s.size());
+}
+
+TEST(MethodTests, Capacity) {
+    String s("cringe");
+
+    ASSERT_EQ(6, s.capacity());
+}
+
+TEST(MethodTests, CapacityEmpty) {
+    String s;
+
+    ASSERT_EQ(0, s.capacity());
+}
+
+TEST(MethodTests, CapacityExtended) {
+    String s("test");
+    s.push_back('c');
+
+    ASSERT_TRUE(s.size() < s.capacity());
+}
+
+TEST(IOTests, InputOneWord) {
+    std::stringstream simulation;
+    const char* text = "test";
+    simulation << text;
+    String s;
+    simulation >> s;
+
+    ASSERT_EQ(String(text), s);
+    check_last_symbol(s);
+}
+
+TEST(IOTests, InputTwoWords) {
+    std::stringstream simulation;
+    simulation << "this is";
+    String s1, s2;
+    simulation >> s1 >> s2;
+
+    ASSERT_EQ(String("this"), s1);
+    ASSERT_EQ(String("is"), s2);
+    check_last_symbol(s1);
+    check_last_symbol(s2);
+}
+
+TEST(IOTests, Output) {
+    std::stringstream simulation;
+    String s1("mytest");
+    simulation << s1;
+
+    String s2;
+    simulation >> s2;
+    ASSERT_EQ(s1, s2);
+    check_last_symbol(s2);
 }
 
 int main(int argc, char** argv) {
--- string.h
+++ string.h
@@ -1,7 +1,7 @@
 #include <algorithm>
+#include <assert>
+#include <cstring>
 #include <iostream>
-
-namespace MyExceptions {
 
 class BaseException {
   public:
@@ -35,39 +35,50 @@ class EmptyStringCallException : public BaseException {
         }
 };
 
-}
-
 class String {
   private:
-    size_t size;
-    size_t capacity;
+    size_t data_size;
+    size_t buffer_size;
     char* buffer;
 
+    void resize_buffer_to(size_t new_buffer_size);
+
+    void copy_data_from_string(const String& source);
+
+    void exception_if_wrong_index(size_t index) const;
+
+    void exception_if_empty_string() const;
+
+    bool matches_substring(const String& foreign, size_t begin_index) const;
+
+    bool is_endl_character(char c) const;
   public:
 
-    String() : size(0), capacity(0), buffer(nullptr)  {}
+    String();
 
     String(char) = delete;
 
-    String(size_t size, char value='\0') 
-            : size(size)
-            , capacity(size + 1)
-            , buffer(new char[capacity]) {
-        std::fill(buffer, buffer + size, value);
-    }
+    String(size_t size, char value='\0');
 
     String(const char* source);
 
     String(const String& source);
 
-    String& operator = (const String& other);
+    String& operator = (const String& source);
 
     String& operator += (const String& other);
 
+    String& operator += (char c);
+    
     friend String operator + (String left, const String& right);
 
+    friend String operator + (String left, char c);
+
+    friend String operator + (char c, const String& right);
 
     bool operator == (const String& other) const;
+
+    bool operator != (const String& other) const;
 
     bool operator < (const String& other) const;
 
@@ -81,13 +92,15 @@ class String {
 
     char operator [] (size_t index) const;
 
-    size_t length() const {
-        return size;
-    }
+    size_t length() const;
 
-    size_t get_capacity() const {
-        return capacity;
-    }
+    char* data();
+
+    const char* data() const;
+    
+    size_t size();
+
+    size_t capacity();
 
     void push_back(char c);
 
@@ -95,19 +108,21 @@ class String {
 
     char& front();
 
-    const char& front() const;
+    char front() const;
 
     char& back();
 
-    const char& back() const;
+    char back() const;
 
     size_t find(const String& substring) const;
 
     size_t rfind(const String& substring) const;
 
-    bool empty() const {
-        return (size != 0);
-    }
+    String substr(size_t from, size_t count) const;
+    
+    bool empty() const;
+
+    void clear();
 
     void shring_to_fit();
 
@@ -115,12 +130,282 @@ class String {
 
     friend std::istream& operator << (std::istream& in, const String& data);
 
-    char* data();
-
-    const char* data() const;
-
-    ~String() {
-        if (buffer != nullptr) delete[] buffer;
-    }
+    ~String();
 };
+
+void String::resize_buffer_to(size_t new_buffer_size) {
+    assert(new_buffer_size > data_size);
+
+    char* new_buffer = new char[new_buffer_size];
+    std::fill(new_buffer, new_buffer + new_buffer_size, '\0');
+    std::copy(buffer, buffer + data_size, new_buffer);
+    
+    delete[] buffer;
+    buffer = new_buffer;
+    buffer_size = new_buffer_size;
+}
+
+void String::copy_data_from_string(const String& source) {
+    data_size = source.data_size;
+    buffer_size = other.buffer_size;
+    buffer = new char[buffer_size];
+
+    std::copy(source.buffer, source.buffer + buffer_size, buffer);
+}
+
+void String::exception_if_wrong_index(size_t index) const {
+    if (index < 0 || data_size <= index) {
+        throw new IndexOutOfRangeException(index, data_size);
+    }
+}
+
+void String::exception_if_empty_string() const {
+    if (empty()) {
+        throw new EmptyStringCallException();
+    }
+}
+
+bool String::matches_substring(const String& foreign, size_t begin_index) const {
+    assert(begin_index + foreign.size() <= size());
+
+    for(size_t i = 0; i < foreign.size(); ++i) {
+        if (buffer[i + begin_index] != foreign[i]) return false;
+    }
+
+    return true;
+}
+
+bool String::is_endl_character(char c) const {
+    return (c == '\n') ||
+        (c == '\r') ||
+        (c == '\0');
+}
+
+String::String(): String(0, '\0');
+
+String::String(size_t size, char value) 
+        : data_size(size)
+        , buffer_size(size + 1)
+        , buffer(new char[buffer_size]) {
+    
+    std::fill(buffer, buffer + data_size);
+    buffer[data_size] = '\0';
+}
+
+String::String(const char* source) 
+        : data_size(strlen(source))
+        , buffer_size(data_size + 1)
+        , buffer(new char[buffer_size]) {
+    
+    std::copy(source, source + data_size, buffer);
+    buffer[data_size] = '\0';
+}
+
+String::String(const String& source) { 
+    copy_data_from_string(other);
+}
+
+String& String::operator = (const String& source) {
+    delete[] buffer;
+    copy_data_from_string(source);
+
+    return *this;
+}
+
+String& String::operator += (const String& other) {
+    size_t new_size = data_size + other.size();
+    if (new_size < capacity()) {
+        // one more byte for \0 at the end
+        resize_buffer_to(new_size + 1);
+    }
+
+    std::copy(other.buffer, other.buffer + other.size(), buffer + data_size);
+    data_size = new_size;
+
+    return *this;
+}
+
+/* There could be:
+ * return *this += String(1, c);
+ * But this generates String from char
+ * then uses more memory allocation
+ * So I decided to write a bit more code :)
+ * */
+String& String::operator += (char c) {
+    if (capacity() == size()) {
+        resize_buffer_to(buffer_size * 2);
+    }
+    buffer[data_size] = c;
+    ++data_size;
+    buffer[data_size] = '\0';
+
+    return *this;
+}
+
+String operator + (String left, const String& right) {
+    return left += right;
+}
+
+String operator + (String left, char right) {
+    return left += right;
+}
+
+String operator + (char left, const String& right) {
+    String result(1, left);
+    return result += right;
+}
+
+bool String::operator == (const String& other) const {
+    if (size() != other.size()) return false;
+    
+    for (size_t i = 0; i < size(); ++i) {
+        if (buffer[i] != other.buffer[i]) return false;
+    }
+
+    return true;
+}
+
+bool String::operator != (const String& other) const {
+    return !(*this == other);
+}
+
+bool String::operator < (const String& other) const {
+    size_t min_size = std::min(size(), other.size());
+
+    for(size_t i = 0; i < min_size; ++i) {
+        if (buffer[i] < other.buffer[i]) return true;
+        if (other.buffer[i] < buffer[i]) return false;
+    }
+
+    return size() < other.size();
+}
+
+bool String::operator > (const String& other) const {
+    return other < (*this);
+}
+
+bool String::operator <= (const String& other) const {
+    return !((*this) > other);
+}
+
+bool String::operator >= (const String& other) const {
+    return !((*this) < other);
+}
+
+
+char& String::operator [] (size_t index) {
+    exception_if_wrong_index(index);
+}
+
+// don't ban me please... it's not copypaste imho))
+char String::operator [] (size_t index) const {
+    exception_if_wrong_index(index);
+    return buffer[index];
+}
+
+size_t String::length() {
+    return size();
+}
+
+const char* String::data() const {
+    return buffer;
+}
+
+char* String::data() {
+    return buffer;
+}
+
+size_t String::size() {
+    return data_size;
+}
+
+size_t String::capacity() {
+    return buffer_size - 1;
+}
+
+void String::push_back(char c) {
+    *this += c;
+}
+
+void String::pop_back() {
+    exception_if_empty_string();
+    data_size--;
+    buffer[data_size] = '\0';
+}
+
+char String::front() const {
+    return (*this)[0];
+}
+
+char& String::front() {
+    return (*this)[0];
+}
+
+char String::back() const {
+    return (*this)[data_size - 1];
+}
+
+char& String::back() {
+    return (*this)[data_size - 1];
+}
+
+size_t String::find(const String& substring) const {
+    if (substring.size() > size()) return size();
+    
+    for (size_t i = 0; i <= size() - substring.size(); ++i) {
+        if (matches_substring(substring, i)) {
+            return i;
+        }
+    }
+    return size();
+} 
+
+size_t String::rfind(const String& substring) const {
+    if (substring.size() > size()) return size();
+
+    for (size_t i = size() - substring.size(); i >= 0; --i) {
+        if (matches_substring(substring, i)) {
+            return i;
+        }
+    }
+    return size();
+}
+
+String String::substr(size_t from, size_t count) const {
+    String result(count, '\0');
+    std::copy(buffer + from, buffer + from + count, result.buffer);
+    return result;
+}
+
+bool String::empty() const {
+    return size() == 0;
+}
+
+void String::clear() {
+    std::fill(buffer, buffer + data_size, '\0');
+    data_size = 0;
+}
+
+void String::shrink_to_fit() {
+    resize_buffer_to(data_size + 1);
+}
+
+std::ostream& operator << (std::ostream& out, const String& data) {
+    for(size_t i = 0; i < size(); ++i) {
+        out << buffer[i];
+    }
+}
+
+std::istream& operator >> (std::istream& in, String& data) {
+    data.clear();
+    
+    char input;
+    while(in.get(input)) {
+        data += input;
+    }
+}
+
+String::~String() {
+    delete[] buffer;
+}
 
